e9ffad589639b08325139f25d64eae37030e7296
Akshit Birla <birlaakshit@gmail.com>
2024-10-03 01:36:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Inputcontainer from "./component/Inputcontainer";
+import Inputcontainer from "./component/InputContainer";
 import TodoContainer from "./component/TodoContainer";
 
 function App() {
